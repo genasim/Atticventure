19460170ab5977d078dc772b9a1b0ac084f263e9
Genadi-Kolev <genadimkolev@gmail.com>
2021-11-19 12:32:43 +0200

    2.2
    -Player animation system improved: now uses Blend trees (без повече спагети)

--- AtticventureProject/Assets/Scripts/Characters Behaviour/Player_Movement.cs
+++ AtticventureProject/Assets/Scripts/Characters Behaviour/Player_Movement.cs
@@ -1,76 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using Pathfinding;
 
 public class Player_Movement : MonoBehaviour
 {
-    [SerializeField] internal Rigidbody2D rb2D;
-    Vector2 movement;
-    public Animator animator;
+    [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private Animator animator;
+    private Vector2 movement;
 
-    public CinemachineVirtualCamera camCinamachine;
+    [SerializeField] private CinemachineVirtualCamera camCinamachine;
 
     public float speed = 2f;
 
-    [SerializeField] private bool isMovingRight;
-    [SerializeField] private bool isMovingLeft;
-    [SerializeField] private bool isMovingUp;
-    [SerializeField] private bool isMovingDown;
-    [SerializeField] private bool isNotMoving;
+    int xAnimation, yAnimation;
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if (movement.x > 0)
-        {
-            isMovingRight = true;
-            isMovingLeft = false;
-            isMovingUp = false;
-            isMovingDown = false;
-            isNotMoving = false;
-        }
-        else if (movement.x < 0)
-        {
-            isMovingLeft = true;
-            isMovingRight = false;
-            isMovingUp = false;
-            isMovingDown = false;
-            isNotMoving = false;
-        }
-        else if (movement.y > 0)
-        {
-            isMovingUp = true;
-            isMovingRight = false;
-            isMovingDown = false;
-            isMovingLeft = false;
-            isNotMoving = false;
-        }
-        else if(movement.y < 0)
-        {
-            isMovingDown = true;
-            isMovingUp = false;
-            isMovingRight = false;
-            isMovingLeft = false;
-            isNotMoving = false;
-        }
-        else
-        {
-            isNotMoving = true;
-            isMovingUp = false;
-            isMovingRight = false;
-            isMovingDown = false;
-            isMovingLeft = false;
-        }
 
-        animator.SetBool("isMovingUp", isMovingUp);
-        animator.SetBool("isMovingRight", isMovingRight);
-        animator.SetBool("isMovingDown", isMovingDown);
-        animator.SetBool("isMovingLeft", isMovingLeft);
-        animator.SetBool("isNotMoving", isNotMoving);
-
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (movement.x != 0 | movement.y != 0)
+        {
+            xAnimation = (int)movement.x;
+            yAnimation = (int)movement.y;
+            animator.SetFloat("Horizontal", xAnimation);
+            animator.SetFloat("Vertical", yAnimation);
+        }
     }
 
     void FixedUpdate()
@@ -80,7 +35,7 @@ public class Player_Movement : MonoBehaviour
 
     void Movement(Vector2 movement)
     {
-        rb2D.MovePosition(rb2D.position + movement * speed * Time.fixedDeltaTime);
+        rb2D.MovePosition(rb2D.position + speed * Time.fixedDeltaTime * movement);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) 
